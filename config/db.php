535778b0cd8756c41e75e62815222e0b42755724
kaustubh-pawar <kaustubh1006p@gmail.com>
2026-03-30 00:58:20 +0530

    fix database connection for railway

--- config/db.php
+++ config/db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "hostel_db");
+
+$conn = new mysqli(
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE']
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
